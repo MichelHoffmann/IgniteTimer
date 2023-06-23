40821b1ffc051ffe3b167d7a59e4fe5d17f15b36
MichelHoffmann <mic.hoff.jr@gmail.com>
2023-06-23 06:25:20 -0500

    aprendendo styled-components e themes

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
 export type ButtonVariant = "primary" | "secondary" | "danger" | "success";
 
@@ -6,7 +6,22 @@ interface ButtonContainerProps {
   variant: ButtonVariant;
 }
 
+const buttonVariants = {
+  primary: "purple",
+  secondary: "orange",
+  danger: "red",
+  success: "green",
+};
+
 export const ButtonContainer = styled.button<ButtonContainerProps>`
   width: 100px;
   height: 40px;
+  border-radius: 4px;
+  border: 0;
+  margin: 8px;
+
+  background-color: ${(props) => props.theme.primary};
+  color: ${(props) => props.theme.white};
+
+  /* ${(props) => `background-color: ${buttonVariants[props.variant]}`} */
 `;
