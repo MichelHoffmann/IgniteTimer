03aee0cdbdb49c8d05fb9c0a4b293181bd5528f0
MichelHoffmann <mic.hoff.jr@gmail.com>
2023-07-29 06:21:37 -0500

    revisando aulas

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -7,6 +7,7 @@ export function Header() {
   return (
     <HeaderContainer>
       <img src={logoIgnite} alt="" />
+
       <nav>
         <NavLink to="/" title="Timer">
           <Timer size={24} />
